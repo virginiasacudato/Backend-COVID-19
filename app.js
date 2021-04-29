9d2cf808b16f7e2db89ff9c37045808c167b1d9d
Virginia Sacudato <virginiasacudato@gmail.com>
2021-04-28 22:56:03 -0300

    Servicio backend Pusher

--- app.js
+++ app.js
@@ -2,23 +2,46 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const Pusher = require('pusher');
+require('dotenv').config();
 
-const users = require('./routes/users');
+//const vote = require('./routes/vote');
 
 const app = express();
 const port = process.env.PORT || 4000;
+// Inicialización de clase pusher
     const pusher = new Pusher({
       appId: process.env.PUSHER_APP_ID,
       key: process.env.PUSHER_KEY,
       secret: process.env.PUSHER_SECRET,
-      cluster: 'eu',
-      encrypted: true,
+      cluster: "us2",
+      useTLS: true
     });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+// Configuración de CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+// Servidor escuchando en puerto
+  app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+  });
 app.use(cookieParser())
 
-app.use('/api/v1/users', users);
+//app.use('/api/v1/vote', vote);
+
+app.post('/api/v1/vote', (req, res) => {
+  const { body } = req; 
+  const { vaccine } = body; // Destructuración de vaccine de body
+  console.log(vaccine);
+  pusher.trigger('covid-api', 'vote', {
+    vaccine,
+  });
+  res.json({ vaccine });
+});
 
 module.exports = app;
